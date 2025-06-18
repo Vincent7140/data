29c8227a7ad6e294438f67922b29a56a3f301a3f
Vincent7140 <113913219+Vincent7140@users.noreply.github.com>
2025-06-18 12:47:29 +0200

    Update Rvert.py

--- Rvert.py
+++ Rvert.py
@@ -1,35 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import rasterio
+import imageio.v3 as iio
 from skimage.transform import resize
 
-# Lire image NeRF (plus petite)
-with rasterio.open("image_nerf.tif") as src_nerf:
-    image_nerf = src_nerf.read([1, 2, 3]).transpose(1, 2, 0).astype(np.float32)
+# Charger les images
+image_nerf = iio.imread("image_nerf.png").astype(np.float32) / 255.0
+image_real = iio.imread("image_real.png").astype(np.float32) / 255.0
 
-# Lire image réelle
-with rasterio.open("image_real.tif") as src_real:
-    image_real = src_real.read([1, 2, 3]).transpose(1, 2, 0).astype(np.float32)
+# Assure qu'on a des RGB uniquement
+if image_nerf.shape[2] > 3:
+    image_nerf = image_nerf[:, :, :3]
+if image_real.shape[2] > 3:
+    image_real = image_real[:, :, :3]
 
-# Redimensionner image réelle vers image_nerf
+# Redimensionne image_real vers image_nerf si tailles différentes
 if image_real.shape != image_nerf.shape:
     image_real = resize(image_real, image_nerf.shape, preserve_range=True, anti_aliasing=True)
 
-# Normalisation
-image_real /= image_real.max()
-image_nerf /= image_nerf.max()
-
-# Calcul différence
+# Calcul des différences RGB
 diff = image_real - image_nerf
 red = np.clip(diff, 0, 1)
 green = np.clip(-diff, 0, 1)
 blue = np.zeros_like(red)
 
-# Empilement RGB
+# Image RGB rouge/vert
 diff_rgb = np.stack([red, green, blue], axis=2)
 
 # Affichage
 plt.imshow(diff_rgb)
-plt.title("Comparaison : Rouge = image_real > image_nerf, Vert = image_nerf > image_real")
+plt.title("Comparaison : Rouge = image réelle > NeRF, Vert = NeRF > image réelle")
 plt.axis('off')
 plt.show()
